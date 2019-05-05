970ce4a881893a4789c29b5cda46337e53984259
Kehashi91 <adam.kaczmarek@gravete.com>
2019-05-06 00:19:17 +0200

    builders expanded, cleanup, fixes and docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,99 +1,151 @@
+//! # RustLoot
+//!
+//! Simple rust cli program to generate random sample of items from a given loottable.
+//! Given a json file with items, and by specifying a list of options, a loot table is build.
+//! Then the roll method will give us a weighted sample from given loottable.
+
+extern crate itertools;
 extern crate rand;
 extern crate serde;
 
-use rand::prelude::*;
+use itertools::Itertools;
 use rand::distributions::WeightedIndex;
-use serde::{Deserialize, Deserializer};
+use rand::prelude::*;
 use serde::de::Error;
+use serde::{Deserialize, Deserializer};
 
 #[derive(Deserialize, Debug)]
 pub struct Item {
-	name: String,
-	value: u16,
-	weight: u16,
-	#[serde(deserialize_with = "tranform_rarity")]
-	rarity: u8,
-	category: String,
+    // Represenation of items in lootable. It is important to distinguish beetween
+    // value and rarity, both are important for rolls but for diffrent purpose
+    name: String,
+    value: u16,
+    weight: u16,
+    #[serde(deserialize_with = "tranform_rarity")]
+    rarity: u8,
+    category: String,
 }
 
-fn tranform_rarity<'de, D>(deserializer: D) -> Result<u8, D::Error> 
-where 
-	D: Deserializer<'de> {
+fn tranform_rarity<'de, D>(deserializer: D) -> Result<u8, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    // We invert the rarity to make bigger rarity to mean that an item is more rare
+    // We have to do that beacouse WeightedIndex inteprets bigger weight (that is
+    // derived from rarity) as more common.
     let deserialized: Result<u8, D::Error> = Deserialize::deserialize(deserializer);
     match deserialized {
-    	Ok(deserialized) => Ok(u8::max_value() - deserialized),
-    	Err(_) => Err(D::Error::custom("Cannot convert rarity!")),
-     }
+        Ok(deserialized) => Ok(u8::max_value() - deserialized),
+        Err(_) => Err(D::Error::custom("Cannot convert rarity!")),
+    }
 }
 
 #[derive(Debug)]
 pub struct LootTable<'a> {
-	biome: &'a str,
-	base_multiplier: u16,
-	items: Vec<&'a Item>,
-	min_value: u16,
+    // Container for items that can be rolled, as well as rules that can influence
+    // rolls (for now only base multiplier)
+    name: &'a str,
+    base_multiplier: u16,
+    items: Vec<&'a Item>,
+    min_value: u16,
 }
 
+#[derive(Debug)]
 pub enum LootTableBuilders<'a> {
-	AddCategory(String),
-	AddItem(&'a str),
-	AddAll,
-	RemoveItem(String),
+    // These are 'commands' that can be used to build a loottable.
+    // todo: build lootables based on YAML or similiar files.
+    AddCategory(&'a str),
+    AddItem(&'a str),
+    AddAll,
+    RemoveItem(&'a str),
 }
 
-impl<'a>  LootTable<'a> {
-	pub fn new(biome: &'a str, items: &'a Vec<Item>, base_multiplier: u16, builders: Vec<LootTableBuilders>) 
-	-> LootTable<'a> {
+impl<'a> LootTable<'a> {
+    pub fn new(
+        name: &'a str,
+        items: &'a Vec<Item>,
+        base_multiplier: u16,
+        builders: Vec<LootTableBuilders>,
+    ) -> LootTable<'a> {
+        // Build a loottable based on vec of LootTableBuilders
 
-		//let local_refs: Vec<&Item> = items.iter().collect();
-		//et items_refs: Vec<&Item> = LootTable::buildershandler(items, &LootTableBuilders::AddAll); // filters to be added
-		let items_refs: Vec<&Item> = LootTable::buildershandler(items, LootTableBuilders::AddItem("cmiks")); // filters to be added
+        let mut items_refs: Vec<&Item> = vec![];
 
+        for builder in builders.into_iter() {
+            LootTable::buildershandler(items, &mut items_refs, builder);
+        }
 
-		let min_value: Vec<u16> = items_refs.iter().map(|x| x.value).collect();
-		let min_value = min_value.into_iter().min().unwrap();
+        let items_refs = LootTable::deduplicate(items_refs); // Ensure no duplicates
 
-		LootTable {
-			biome: biome,
-			items: items_refs,
-			base_multiplier: base_multiplier,
-			min_value: min_value
-		}
-	}
+        let min_value: Vec<u16> = items_refs.iter().map(|x| x.value).collect();
+        let min_value = min_value.into_iter().min().unwrap(); // get value of least valuable item
 
-	pub fn roll(&self, score: u16, random_thread: & mut rand::prelude::ThreadRng) -> Vec<&Item> {
-		
-		let mut total_value = score as f32 * self.base_multiplier as f32 * random_thread.gen_range(0.8, 1.2);
-		let mut loot: Vec<&Item> = vec![];
+        LootTable {
+            name: name,
+            items: items_refs,
+            base_multiplier: base_multiplier,
+            min_value: min_value,
+        }
+    }
 
-		while total_value >= self.min_value as f32 {
-			let mut temp_items: Vec<_> = self.items
-				.clone()
-				.into_iter()
-				.filter(|item| item.value <= total_value as u16)
-				.collect();
+    pub fn roll(&self, score: u16, random_thread: &mut rand::prelude::ThreadRng) -> Vec<&Item> {
+        // Get a loot from a given roll. Score is a roll value from a player, which determines the total value
+        // of items in a roll. What items will be in the loot is determined by items rarity.
 
-			let weights: Vec<u32> = temp_items.iter().map(|item| item.rarity as u32).collect();
-			let dist = WeightedIndex::new(&weights).unwrap();
+        let mut loot_value =
+            score as f32 * self.base_multiplier as f32 * random_thread.gen_range(0.8, 1.2);
+        let mut loot: Vec<&Item> = vec![];
 
-			println!("{:?}", temp_items);
+        while loot_value >= self.min_value as f32 {
+            let temp_items: Vec<_> = self
+                .items
+                .clone()
+                .into_iter()
+                .filter(|item| item.value <= loot_value as u16)
+                .collect();
 
-			let index = dist.sample(random_thread);
-			loot.push(temp_items[index]);
-			total_value -= temp_items[index].value as f32;
+            let weights: Vec<u32> = temp_items.iter().map(|item| item.rarity as u32).collect();
+            let dist = WeightedIndex::new(&weights).unwrap();
 
-			println!("{:?}", total_value);
-		}
+            println!("{:?}", temp_items);
 
-		loot
-	}
+            let index = dist.sample(random_thread);
+            loot.push(temp_items[index]);
+            loot_value -= temp_items[index].value as f32;
+        }
 
-	fn buildershandler<'b>(items: &'b Vec< Item>, builder_options: LootTableBuilders ) -> Vec<&'b Item> {
-		match builder_options {
-			LootTableBuilders::AddCategory(c) => items.iter().collect(),
-			LootTableBuilders::AddItem(name) => items.iter().filter(|item| item.name == *name).collect(),
-			LootTableBuilders::AddAll => items.iter().collect(),
-			LootTableBuilders::RemoveItem(item) => items.iter().collect(),
-		}
-	}
+        loot
+    }
+
+    fn buildershandler<'b>(
+        items: &'b Vec<Item>,
+        current_items: &'a mut Vec<&'b Item>,
+        builder_options: LootTableBuilders,
+    ) {
+        match builder_options {
+            LootTableBuilders::AddCategory(category) => {
+                items
+                    .iter()
+                    .filter(|item| item.category == *category)
+                    .for_each(|item| current_items.push(item));
+            }
+            LootTableBuilders::AddItem(name) => {
+                items
+                    .iter()
+                    .filter(|item| item.name == *name)
+                    .for_each(|item| current_items.push(item));
+            }
+            LootTableBuilders::AddAll => {
+                items.iter().for_each(|item| current_items.push(item));
+            }
+            LootTableBuilders::RemoveItem(_) => unimplemented!(),
+        }
+    }
+
+    fn deduplicate<'b>(current_items: Vec<&'b Item>) -> Vec<&Item> {
+        current_items
+            .into_iter()
+            .unique_by(|item| &item.name)
+            .collect()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rustloot::{LootTable, Item, LootTableBuilders};
+use rustloot::{Item, LootTable, LootTableBuilders};
 
 use std::error::Error;
 use std::fs::File;
@@ -6,27 +6,26 @@ use std::io::BufReader;
 use std::path::Path;
 
 fn main() {
-	let mut rng = rand::thread_rng();
-	let items = read_items("items.json").unwrap();
+    let mut rng = rand::thread_rng();
+    let items = read_items("items.json").unwrap();
 
-	let builders = vec![LootTableBuilders::AddAll];
-	let builders2 = vec![LootTableBuilders::AddAll];
+    let builders = vec![LootTableBuilders::AddCategory("misc")];
+    let builders2 = vec![LootTableBuilders::AddAll];
 
-	let table = LootTable::new("miasto", &items, 2, builders);
-	println!("{:?}", table.roll(15, & mut rng));
-	println!("{:?}", table.roll(31, & mut rng));
-	let table2 = LootTable::new("miast2o", &items, 3, builders2);
-	println!("{:?}", table2.roll(15, & mut rng));
-	println!("{:?}", table2.roll(31, & mut rng));
+    let table = LootTable::new("test", &items, 2, builders);
+    println!("{:?}", table.roll(15, &mut rng));
+    println!("{:?}", table.roll(31, &mut rng));
+    let table2 = LootTable::new("test2", &items, 3, builders2);
+    println!("{:?}", table2.roll(15, &mut rng));
+    println!("{:?}", table2.roll(31, &mut rng));
 }
 
 fn read_items<P: AsRef<Path>>(path: P) -> Result<Vec<Item>, Box<Error>> {
-	let file = File::open(path)?;
-	let reader = BufReader::new(file);
+    // Read the json file with items
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
 
-	let items = serde_json::from_reader(reader)?;
+    let items = serde_json::from_reader(reader)?;
 
-	Ok(items)
+    Ok(items)
 }
-
-
